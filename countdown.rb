f2b7eba84a43e6f7f9b4c262d3d32635f7d58a56
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 18:33:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,10 @@ def countdown (number)
     number -= 1
   end
 end
+ def countdown_with_sleep(number)
+   while number > 0
+     return "HAPPY NEW YEAR!"
+     number -= 1
+     sleep(1)
+   end
+ end
